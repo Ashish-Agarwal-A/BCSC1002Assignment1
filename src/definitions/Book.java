1cfeb1a9e66f490518cd89868a8c002c6e9fd647
Ashish-Agarwal-A <ashish.agarwal_cs19@gla.ac.in>
2020-08-30 13:16:37 +0530

    add fields for Library.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,6 +1,6 @@
 /*  Created by IntelliJ IDEA.
  *  User: Ashish Agarwal (Ashish-Agarwal-A)
- *  Date: 31/08/20
+ *  Date: 30/08/20
  *  Time: 3:49 PM
  *  File Name : Book.java
  * */
--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -1,10 +1,13 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: Ashish Agarwal (Ashish-Agarwal-A)
+ *  Date: 30/08/20
  *  Time: 3:50 PM
  *  File Name : Library.java
  * */
 package definitions;
 
 public class Library {
+    private String[] Book;
+
+
 }
