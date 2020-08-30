9844438efbfc290baae8c16a9554a5b1eda29902
Ashish-Agarwal-A <ashish.agarwal_cs19@gla.ac.in>
2020-08-30 12:11:58 +0530

    add fields for Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,16 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+ *  User: Ashish Agarwal (Ashish-Agarwal-A)
+ *  Date: 31/08/20
  *  Time: 3:49 PM
  *  File Name : Book.java
  * */
 package definitions;
 
+import java.util.Objects;
+
 public class Book {
+    private String bookName;
+    private String authorName;
+    private String ISBNNumberOfBook;
 }
+
